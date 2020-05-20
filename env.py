9c5a5d4205e4d5803a06f74a1cf72bafe04dec9e
Cam Allen <csal@brown.edu>
2020-05-19 23:45:27 -0400

    Fix bug where EpisodicLife was wrapping test env

--- env.py
+++ env.py
@@ -7,15 +7,19 @@ from gym_wrappers import FrameStack, MaxAndSkipEnv, AtariPreprocess, ResetARI, \
     TorchWrapper, EpisodicLifeEnv
 
 
-def make_atari(env, num_frames):
+def make_atari(env, num_frames, training=False):
     """ Wrap env in atari processed env """
-    return FrameStack(MaxAndSkipEnv(AtariPreprocess(EpisodicLifeEnv(env)), 4),
+    if training:
+        env = EpisodicLifeEnv(env)
+    return FrameStack(MaxAndSkipEnv(AtariPreprocess(env), 4),
                       num_frames)
 
 
-def make_ari(env, num_frames):
+def make_ari(env, num_frames, training=False):
     """ Wrap env in reset to match observation """
-    return FrameStack(ResetARI(AtariARIWrapper(EpisodicLifeEnv(env))), num_frames)
+    if training:
+        env = EpisodicLifeEnv(env)
+    return FrameStack(ResetARI(AtariARIWrapper(env)), num_frames)
 
 
 def get_env(args):
@@ -35,14 +39,14 @@ def get_env(args):
         uuid_tag = args.uuid
 
     if args.architecture == 'ram':
-        env = FrameStack(env, args.history_length)
-        test_env = FrameStack(test_env, args.history_length)
+        env = FrameStack(env, args.history_length, training=True)
+        test_env = FrameStack(test_env, args.history_length, training=False)
     elif args.architecture == 'ari':
-        env = make_ari(env, args.history_length)
-        test_env = make_ari(test_env, args.history_length)
+        env = make_ari(env, args.history_length, training=True)
+        test_env = make_ari(test_env, args.history_length, training=False)
     else:
-        env = make_atari(env, args.history_length)
-        test_env = make_atari(test_env, args.history_length)
+        env = make_atari(env, args.history_length, training=True)
+        test_env = make_atari(test_env, args.history_length, training=False)
 
     # env = TorchWrapper(env, args.device)
     # test_env = TorchWrapper(env, args.device)
@@ -54,97 +58,3 @@ def get_env(args):
     run_tag += '_seed_' + str(args.seed)
 
     return env, test_env, run_tag
-
-
-# class Env():
-#     def __init__(self, args):
-#         self.device = args.device
-#         self.ale = atari_py.ALEInterface()
-#         self.ale.setInt('random_seed', args.seed)
-#         self.ale.setInt('max_num_frames_per_episode', args.max_episode_length)
-#         self.ale.setFloat('repeat_action_probability',
-#                           0)  # Disable sticky actions
-#         self.ale.setInt('frame_skip', 0)
-#         self.ale.setBool('color_averaging', False)
-#         self.ale.loadROM(atari_py.get_game_path(
-#             args.game))  # ROM loading must be done after setting options
-#         actions = self.ale.getMinimalActionSet()
-#         self.actions = dict([i, e]
-#                             for i, e in zip(range(len(actions)), actions))
-#         self.lives = 0  # Life counter (used in DeepMind training)
-#         self.life_termination = False  # Used to check if resetting only from loss of life
-#         self.window = args.history_length  # Number of frames to concatenate
-#         self.state_buffer = deque([], maxlen=args.history_length)
-#         self.training = True  # Consistent with model training mode
-
-#     def _get_state(self):
-#         state = cv2.resize(self.ale.getScreenGrayscale(), (84, 84),
-#                            interpolation=cv2.INTER_LINEAR)
-#         return torch.tensor(state, dtype=torch.float32,
-#                             device=self.device).div_(255)
-
-#     def _reset_buffer(self):
-#         for _ in range(self.window):
-#             self.state_buffer.append(torch.zeros(84, 84, device=self.device))
-
-#     def reset(self):
-#         if self.life_termination:
-#             self.life_termination = False  # Reset flag
-#             self.ale.act(0)  # Use a no-op after loss of life
-#         else:
-#             # Reset internals
-#             self._reset_buffer()
-#             self.ale.reset_game()
-#             # Perform up to 30 random no-ops before starting
-#             for _ in range(random.randrange(30)):
-#                 self.ale.act(0)  # Assumes raw action 0 is always no-op
-#                 if self.ale.game_over():
-#                     self.ale.reset_game()
-#         # Process and return "initial" state
-#         observation = self._get_state()
-#         self.state_buffer.append(observation)
-#         self.lives = self.ale.lives()
-#         return torch.stack(list(self.state_buffer), 0)
-
-#     def step(self, action):
-#         # Repeat action 4 times, max pool over last 2 frames
-#         frame_buffer = torch.zeros(2, 84, 84, device=self.device)
-#         reward, done = 0, False
-#         for t in range(4):
-#             reward += self.ale.act(self.actions.get(action))
-#             if t == 2:
-#                 frame_buffer[0] = self._get_state()
-#             elif t == 3:
-#                 frame_buffer[1] = self._get_state()
-#             done = self.ale.game_over()
-#             if done:
-#                 break
-#         observation = frame_buffer.max(0)[0]
-#         self.state_buffer.append(observation)
-#         # Detect loss of life as terminal in training mode
-#         if self.training:
-#             lives = self.ale.lives()
-#             if lives < self.lives and lives > 0:  # Lives > 0 for Q*bert
-#                 self.life_termination = not done  # Only set flag when not truly done
-#                 done = True
-#             self.lives = lives
-#         # Return state, reward, done
-#         return torch.stack(list(self.state_buffer), 0), reward, done
-
-#     # Uses loss of life as terminal signal
-#     def train(self):
-#         self.training = True
-
-#     # Uses standard terminal signal
-#     def eval(self):
-#         self.training = False
-
-#     def action_space(self):
-#         return len(self.actions)
-
-#     def render(self):
-#         cv2.imshow('screen', self.ale.getScreenRGB()[:, :, ::-1])
-#         cv2.waitKey(1)
-
-#     def close(self):
-#         cv2.destroyAllWindows()
